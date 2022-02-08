1a25b0f60e98d69ad84612a8f629f5ef7c84306c
SumanthEntivenka <98640786+SumanthEntivenka@users.noreply.github.com>
2022-02-08 19:17:43 +0530

    Update JenkinsDemo.java

--- JenkinsDemo.java
+++ JenkinsDemo.java
@@ -1,5 +1,5 @@
 class JenkinsDemo{
 public static void main(String args[]){
-  System.out.println("My first jenkins");
+  System.out.println("Hurray!! your done if your output is Finished :SUCCESS");
   }
 }
